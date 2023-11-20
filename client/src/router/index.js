37488f9869d6d7f9696ed6f92ed01ad2698cf8c4
jayoung977 <wjsrnr20@gmail.com>
2023-11-20 17:46:07 +0900

    fix : 이미지 업로드 로직 수정

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,101 +4,100 @@ import TheMainView from "../views/TheMainView.vue";
 import TheElectricChargingStation from "@/views/TheElectricChargingStation.vue";
 import ScheduleHome from "@/components/schedule/ScheduleHome.vue";
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "main",
-      component: TheMainView,
-    },
-    {
-      path: "/estations",
-      name: "estations",
-      component: TheElectricChargingStation,
-    },
-    {
-      path: "/user",
-      name: "user",
-      component: () => import("@/views/TheUserView.vue"),
-      children: [
-        {
-          path: "login",
-          name: "user-login",
-          component: () => import("@/components/users/UserLogin.vue"),
-        },
-        {
-          path: "join",
-          name: "user-join",
-          component: () => import("@/components/users/UserRegister.vue"),
-        },
-        {
-          path: "mypage",
-          name: "user-mypage",
-          component: () => import("@/components/users/UserMyPage.vue"),
-        },
-        // {
-        //   path: "modify/:userid",
-        //   name: "user-modify",
-        //   component: () => import("@/components/users/UserModify.vue"),
-        // },
-      ],
-    },
-    {
-      path: "/board",
-      name: "board",
-      // component: TheBoardView,
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/TheBoardView.vue"),
-      redirect: { name: "article-list" },
-      children: [
-        {
-          path: "list",
-          name: "article-list",
-          component: () => import("@/components/board/BoardList.vue"),
-        },
-        {
-          path: "view/:articleno",
-          name: "article-view",
-          component: () => import("@/components/board/BoardDetail.vue"),
-        },
-        {
-          path: "write",
-          name: "article-write",
-          component: () => import("@/components/board/BoardWrite.vue"),
-        },
-        {
-          path: "modify/:articleno",
-          name: "article-modify",
-          component: () => import("@/components/board/BoardModify.vue"),
-        },
-      ],
-    },
-    {
-      path: "/schedule",
-      name: "schedule",
-      component: ScheduleHome,
-      children: [
-        {
-          path: "list",
-          name: "attraction-list",
-          component: () => import("@/components/schedule/AttractionList.vue"),
-        },
-        {
-          path: "date",
-          name: "trip-date",
-          component: () => import("@/components/schedule/TripDate.vue"),
-        },
-        {
-          path: "add-to-list",
-          name: "add-schedule",
-          component: () =>
-            import("@/components/schedule/AddAttractionModal.vue"),
-        },
-      ],
-    },
-  ],
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/",
+			name: "main",
+			component: TheMainView,
+		},
+		{
+			path: "/estations",
+			name: "estations",
+			component: TheElectricChargingStation,
+		},
+		{
+			path: "/user",
+			name: "user",
+			component: () => import("@/views/TheUserView.vue"),
+			children: [
+				{
+					path: "login",
+					name: "user-login",
+					component: () => import("@/components/users/UserLogin.vue"),
+				},
+				{
+					path: "join",
+					name: "user-join",
+					component: () => import("@/components/users/UserRegister.vue"),
+				},
+				{
+					path: "mypage",
+					name: "user-mypage",
+					component: () => import("@/components/users/UserMyPage.vue"),
+				},
+				// {
+				//   path: "modify/:userid",
+				//   name: "user-modify",
+				//   component: () => import("@/components/users/UserModify.vue"),
+				// },
+			],
+		},
+		{
+			path: "/board",
+			name: "board",
+			// component: TheBoardView,
+			// route level code-splitting
+			// this generates a separate chunk (About.[hash].js) for this route
+			// which is lazy-loaded when the route is visited.
+			component: () => import("../views/TheBoardView.vue"),
+			redirect: { name: "article-list" },
+			children: [
+				{
+					path: "list",
+					name: "article-list",
+					component: () => import("@/components/board/BoardList.vue"),
+				},
+				{
+					path: "view/:articleno",
+					name: "article-view",
+					component: () => import("@/components/board/BoardDetail.vue"),
+				},
+				{
+					path: "write",
+					name: "article-write",
+					component: () => import("@/components/board/BoardWrite.vue"),
+				},
+				{
+					path: "modify/:articleno",
+					name: "article-modify",
+					component: () => import("@/components/board/BoardModify.vue"),
+				},
+			],
+		},
+		{
+			path: "/schedule",
+			name: "schedule",
+			component: ScheduleHome,
+			children: [
+				{
+					path: "list",
+					name: "attraction-list",
+					component: () => import("@/components/schedule/AttractionList.vue"),
+				},
+				{
+					path: "date",
+					name: "trip-date",
+					component: () => import("@/components/schedule/TripDate.vue"),
+				},
+				{
+					path: "add-to-list",
+					name: "add-schedule",
+					component: () => import("@/components/schedule/AddAttractionModal.vue"),
+				},
+			],
+		},
+	],
 });
 
 export default router;

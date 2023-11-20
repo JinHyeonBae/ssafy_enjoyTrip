246b3fd996fa8f988c5bda69f5e52b00bb1923d1
jayoung977 <wjsrnr20@gmail.com>
2023-11-20 09:04:17 +0900

    feat:로그인 기능 구현 client 및 server 에서token활용한 로그인 기능 구현

--- client/src/components/users/UserLogin.vue
+++ client/src/components/users/UserLogin.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import { ref } from "vue";
 import { storeToRefs } from "pinia";
 import { useRouter } from "vue-router";
@@ -31,51 +31,82 @@ const login = async () => {
 	}
 	router.push("/");
 };
-</script>
+</script> -->
+<script setup>
+import { ref } from "vue";
+import { storeToRefs } from "pinia";
+import { useRouter } from "vue-router";
+import { useMemberStore } from "@/stores/member";
+import { useMenuStore } from "@/stores/menu";
 
+const router = useRouter();
+
+const memberStore = useMemberStore();
+
+const { isLogin } = storeToRefs(memberStore);
+const { userLogin, getUserInfo } = memberStore;
+const { changeMenuState } = useMenuStore();
+
+const loginUser = ref({
+  userId: "",
+  userPwd: "",
+});
+
+const login = async () => {
+  await userLogin(loginUser.value);
+  let token = sessionStorage.getItem("accessToken");
+  if (isLogin) {
+    getUserInfo(token);
+    changeMenuState();
+  }
+  router.push("/");
+};
+</script>
 <template>
-	<div
-		class="container my-5 d-flex w-100 align-items-center justify-content-center"
-		style="height: 50vh"
-	>
-		<!-- <div>	<div class="row justify-content-center">
+  <div
+    class="container my-5 d-flex w-100 align-items-center justify-content-center"
+    style="height: 50vh"
+  >
+    <!-- <div>	<div class="row justify-content-center">
 				<h2 class="my-3 py-3 shadow-sm bg-light text-center">로그인</h2>
 			</div> -->
 
-		<form class="w-25">
-			<div class="form-check mb-3 float-end">
-				<input class="form-check-input" type="checkbox" />
-				<label class="form-check-label" for="saveid"> 아이디저장 </label>
-			</div>
-			<div class="mb-3 text-start">
-				<label for="userid" class="form-label">아이디</label>
-				<input
-					type="text"
-					class="form-control bg-body-tertiary"
-					v-model="loginUser.userId"
-					placeholder=""
-				/>
-			</div>
-			<div class="mb-3 text-start">
-				<label for="userpwd" class="form-label">비밀번호</label>
-				<input
-					type="password"
-					class="form-control bg-body-tertiary"
-					v-model="loginUser.userPwd"
-					@keyup.enter="login"
-					placeholder=""
-				/>
-			</div>
-			<div class="col-auto text-center">
-				<button type="button" class="btn btn-primary w-100" @click="login">로그인</button>
-				<!-- <button type="button" class="btn btn-outline-success ms-1 mb-3">회원가입</button> -->
-			</div>
-			<div class="my-2">
-				아직 회원이 아니세요?
-				<router-link :to="{ name: 'user-join' }"> 회원가입하기 </router-link>
-			</div>
-		</form>
-	</div>
+    <form class="w-25">
+      <div class="form-check mb-3 float-end">
+        <input class="form-check-input" type="checkbox" />
+        <label class="form-check-label" for="saveid"> 아이디저장 </label>
+      </div>
+      <div class="mb-3 text-start">
+        <label for="userid" class="form-label">아이디</label>
+        <input
+          type="text"
+          class="form-control bg-body-tertiary"
+          v-model="loginUser.userId"
+          placeholder=""
+        />
+      </div>
+      <div class="mb-3 text-start">
+        <label for="userpwd" class="form-label">비밀번호</label>
+        <input
+          type="password"
+          class="form-control bg-body-tertiary"
+          v-model="loginUser.userPwd"
+          @keyup.enter="login"
+          placeholder=""
+        />
+      </div>
+      <div class="col-auto text-center">
+        <button type="button" class="btn btn-primary w-100" @click="login">
+          로그인
+        </button>
+        <!-- <button type="button" class="btn btn-outline-success ms-1 mb-3">회원가입</button> -->
+      </div>
+      <div class="my-2">
+        아직 회원이 아니세요?
+        <router-link :to="{ name: 'user-join' }"> 회원가입하기 </router-link>
+      </div>
+    </form>
+  </div>
 </template>
 
 <style scoped></style>
